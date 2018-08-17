ad6ba9cbe4fed79501be1da7010c0e1ae0c8718a
asadullah <tylerlong19@gmail.com>
2018-08-17 14:55:54 +0500

    space in parent branch

--- src/main/java/main/VrooziApplication.java
+++ src/main/java/main/VrooziApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VrooziApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(VrooziApplication.class, args);
     }
