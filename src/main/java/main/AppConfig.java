df9ba52dc9168e9155e0d64bf54e0da9fd2dc4bb
asadullah <tylerlong19@gmail.com>
2018-08-21 18:21:58 +0500

    localization via thymeleaf

--- src/main/java/main/AppConfig.java
+++ src/main/java/main/AppConfig.java
@@ -1,19 +1,25 @@
 package main;
 
+import java.util.Collections;
 import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -65,5 +71,59 @@ public class AppConfig extends HandlerInterceptorAdapter {
     resourceBundle.setAlwaysUseMessageFormat(true);
     return resourceBundle;
   }
+
+
+  /***********************start of thymeleaf template resolver*********************************/
+  @Bean
+  @Qualifier("htmlTemplateEngine")
+  public TemplateEngine emailTemplateEngine() {
+    final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.addTemplateResolver(textTemplateResolver());
+    templateEngine.addTemplateResolver(htmlTemplateResolver());
+    //templateEngine.addTemplateResolver(stringTemplateResolver());
+    templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+    return templateEngine;
+  }
+
+  private ITemplateResolver textTemplateResolver() {
+    final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setOrder(Integer.valueOf(1));
+    templateResolver.setResolvablePatterns(Collections.singleton("text/*"));
+    templateResolver.setPrefix("templates/");
+    templateResolver.setSuffix(".txt");
+    templateResolver.setTemplateMode(TemplateMode.TEXT);
+    templateResolver.setCharacterEncoding("UTF-8");
+    templateResolver.setCacheable(false);
+    return templateResolver;
+  }
+
+  private ITemplateResolver htmlTemplateResolver() {
+    final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+    templateResolver.setOrder(Integer.valueOf(2));
+    templateResolver.setResolvablePatterns(Collections.singleton("*"));
+    templateResolver.setPrefix("templates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCharacterEncoding("UTF-8");
+    templateResolver.setCacheable(false);
+    return templateResolver;
+  }
+
+  @Bean
+  public ResourceBundleMessageSource emailMessageSource() {
+    final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("emailmessages");
+    return messageSource;
+  }
+
+  /*private ITemplateResolver stringTemplateResolver() {
+    final StringTemplateResolver templateResolver = new StringTemplateResolver();
+    templateResolver.setOrder(Integer.valueOf(3));
+    // No resolvable pattern, will simply process as a String template everything not previously matched
+    templateResolver.setTemplateMode("HTML5");
+    templateResolver.setCacheable(false);
+    return templateResolver;
+  }*/
+  /**************************end of thymeleaf template resolver******************************/
 
 }
--- src/main/java/main/rest/VrooziRestController.java
+++ src/main/java/main/rest/VrooziRestController.java
@@ -11,45 +11,65 @@ import main.util.UTF8Control;
 import main.vo.User;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @RestController
 @RequestMapping(value = "/api")
 public class VrooziRestController implements SetupData {
-    @Autowired
-    ReloadableResourceBundleMessageSource messageSource;
+  @Autowired
+  ReloadableResourceBundleMessageSource messageSource;
 
-    @Autowired
-    private static VelocityEngine velocityEngine;
+  @Autowired
+  private static VelocityEngine velocityEngine;
 
-    @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
-    public String helloWorld (@RequestHeader(value="Accept-Language") String locale) {
+  @Autowired
+  @Qualifier("htmlTemplateEngine")
+  private TemplateEngine htmlTemplateEngine;
 
-        /*Map<String, Object> model = new HashMap<String, Object>();
-        model.put("title", "title");
-        model.put("body", "body");
-        String text = VelocityEngineUtils
-            .mergeTemplateIntoString(velocityEngine, "email.vm", "UTF-8", model);*/
+  @RequestMapping(value = "/helloworld", method = RequestMethod.GET)
+  public String helloWorld (@RequestHeader(value="Accept-Language") String locale) {
+
+      /*Map<String, Object> model = new HashMap<String, Object>();
+      model.put("title", "title");
+      model.put("body", "body");
+      String text = VelocityEngineUtils
+          .mergeTemplateIntoString(velocityEngine, "email.vm", "UTF-8", model);*/
 
 
-      /*ResourceBundle resourceBundle = ResourceBundle.getBundle("emailmessages",new Locale(locale));
-      System.out.println(resourceBundle.getString("resource.msg"));*/
-      ResourceBundle bundle = ResourceBundle.getBundle("emailmessages", new Locale(locale), new UTF8Control());
-      return bundle.getString("resource.msg");
-    }
+    /*ResourceBundle resourceBundle = ResourceBundle.getBundle("emailmessages",new Locale(locale));
+    System.out.println(resourceBundle.getString("resource.msg"));*/
+    ResourceBundle bundle = ResourceBundle.getBundle("emailmessages", new Locale(locale), new UTF8Control());
+    return bundle.getString("resource.msg");
+  }
 
-    @RequestMapping(value = "/userlist", produces = "application/json", method = RequestMethod.POST)
-    public List<User> getUserList(User user) {
+  @RequestMapping(value = "/userlist", produces = "application/json", method = RequestMethod.POST)
+  public List<User> getUserList(User user) {
 
-        List<User> users = setupUserList();
-        return users.stream()
-            .filter(usr -> user.getId().equals(usr.getId()))
-            .collect(Collectors.toList());
-    }
+      List<User> users = setupUserList();
+      return users.stream()
+          .filter(usr -> user.getId().equals(usr.getId()))
+          .collect(Collectors.toList());
+  }
+
+  @RequestMapping(value = "/thymeleaf", method = RequestMethod.GET)
+  @ResponseBody
+  public String localizedEmail() {
+    return build("this is message");
+  }
+
+  private String build(String message) {
+    final Context ctx = new Context(new Locale("ja"));
+    ctx.setVariable("mesasge", message);
+    return this.htmlTemplateEngine.process("html/mailTemplate", ctx);
+  }
 
 }
